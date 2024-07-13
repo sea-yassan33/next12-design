49fd2b10b1b988558caa0f4cf3104a88dc8e91a9
sea-yassan33 <a.yasuda_tr51@me.com>
2024-07-13 19:03:30 +0900

    update240713

--- README.md
+++ README.md
@@ -5,6 +5,9 @@
 - [UIフレームワーク](./readme_docment/01_devSet.md#uiフレームワーク)
 - [Next.js各種設定](./readme_docment/01_devSet.md#nextjsの各種設定)
 
+## top
+![top_display](https://i.gyazo.com/fc7702c9627e80e2da26efed53b64918.png)
+
 ## sample01
 ![sample1](https://i.gyazo.com/ab3e7dbcbf047943d72849ee49f68b2e.png)
 
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,78 @@
+import React,{useState} from 'react';
 import homeStyles from '../styles/Home.module.css'
 import Image from "next/image";
 import Link from 'next/link';
 
+
 const styles ={
   navStyle : "navbar navbar-expand-lg navbar-dark pd-back-color", 
   cardFoot: "card-footer text-end small text-muted",
+  btnPart01: "btn rounded-pill px-3",
+  btnpart02: "btn dropdown-toggle dropdown-toggle-split show",
 };
 
-
 export default function Home() {
+  // ドロップダウンメニューの作成
+  const [dropOpen, setDropOpen] = useState(false);
+  const toggleDropdown = () => {
+    setDropOpen(!dropOpen)
+  };
+
   return (
     <div>
       <nav className={`${styles.navStyle} ${homeStyles.pdBgcolor}`}>
         <div className={'container-fluid'}>
           <a className={`navbar-brand`} href="#">
             <Image src="https://i.gyazo.com/e4e71335ce331b27a3e3151ff99dccfe.png" width="60" height="60" className="d-inline-block align-top mr-1" alt="Logo"/>
-            デザイン集
+            デザイン・テンプレート集
           </a>
         </div>
       </nav>
       <main className={'container-fluid'}>
-        
-        {/* 作成後削除 */}
         <div className={`row`}>
+          {/* 右側 */}
           <div className={`col-3 ${homeStyles.pdSideMenu}`}>
-            <p>サイドメニュー<br/>(リンク集)</p>
+            <div className='mt-3 text-center'>
+              <p className={`h2 ${homeStyles.pdFontComic}`}>Link Collection</p>
+            </div>
+            <hr/>
+            <div className='mt-3'>
+              <p className={`h3 ${homeStyles.pdFontComic}`}>Boot Strap</p>
+              <div className='ms-3 mb-2'>
+                <Link href="https://getbootstrap.jp/docs/5.3/examples/" target='_blank'>
+                  <button className={`${styles.btnPart01} btn-warning`}>サンプル</button>
+                </Link>
+              </div>
+              <div className='ms-3 mb-2'>
+                <Link href="https://bootstrap-guide.com/sample/cheatsheet" target='_blank'>
+                  <button className={`${styles.btnPart01} btn-warning`}>チートシート</button>
+                </Link>
+              </div>
+              <div className='ms-3 mb-2'>
+                <Link href="https://bootstrap-guide.com/example" target='_blank'>
+                  <button className={`${styles.btnPart01} btn-warning`}>設置ガイド</button>
+                </Link>
+              </div>
+              <div className='ms-3 mb-2'>
+                <Link href="https://bootstraptaste.com/" target='_blank'>
+                  <button className={`${styles.btnPart01} btn-warning`}>DS:bootstraptaste</button>
+                </Link>
+              </div>
+              <div className='btn-group mt-3'>
+                <button type='button' className='btn btn-secondary'>クラス名</button>
+                <button type='button' className={`${styles.btnpart02} btn-secondary`} onClick={toggleDropdown} aria-expanded={dropOpen}>
+                  <span className='visually-hidden'>ドロップダウンの切替</span>
+                </button>
+                <ul className={`dropdown-menu ${dropOpen ? 'show' : ''} ${homeStyles.pdBtnPullDown}`} data-popper-placement="bottom-start">
+                  <li><Link href="https://qiita.com/Rock22/items/e4e89f15c29e1415977d" className='dropdown-item' target='_blank'>text-align</Link></li>
+                  <li><Link href="#" className='dropdown-item'>メニュー１</Link></li>
+                </ul>
+              </div>
+              <hr/>
+            </div>
+            
           </div>
+          {/* 左側 */}
           <div className={`col-9 ${homeStyles.pdMainMenu}`}>
             <h1>デザイン・テンプレート集</h1>
             <div className='card-group mb-3'>
